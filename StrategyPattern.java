5e8e40b07d89b001dfa11470a4d0b9ce9cd2f920
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2019-07-29 10:47:03 +0530

    Update StrategyPattern.java

--- StrategyPattern.java
+++ StrategyPattern.java
@@ -83,3 +83,99 @@ public class StrategyPattern {
     }
 
 }
+// calculation example
+
+package com.company;
+
+interface  Operationable{
+
+    int doOparation(int x , int y);
+}
+
+class Add implements  Operationable{
+
+    @Override
+    public int doOparation(int x , int y){
+        return x+y;
+    }
+
+}
+
+class  Sub implements  Operationable{
+
+    @Override
+    public int doOparation(int x , int y){
+        return x-y;
+    }
+}
+
+class Mul implements  Operationable{
+
+    @Override
+    public int doOparation(int x , int y){
+        return x*y;
+    }
+}
+
+class Div implements Operationable{
+
+    @Override
+    public int doOparation(int x , int y){
+        return x/y;
+    }
+}
+
+class  Context{
+
+    private  Operationable operationable;
+
+   Context(Operationable operationable){
+        this.operationable = operationable;
+    }
+
+    public int doCal(int x , int y){
+       return operationable.doOparation(x,y);
+    }
+
+}
+
+public class CalculationMain {
+
+    public static void main(String[] args) {
+
+        //I need to add two numbers
+        
+        // without strategy design pattern
+        int x = 30 ; int y = 15;
+        Add add = new Add();
+        System.out.println(add.doOparation(x,y));
+
+        // I need to Subtitute
+        Sub sub = new Sub();
+        System.out.println(sub.doOparation(x,y));
+
+        // I need to multiply
+        Mul mul = new Mul();
+        System.out.println(mul.doOparation(x,y));
+
+        //I need to Div
+        Div div = new Div();
+        System.out.println(sub.doOparation(x,y));
+
+        // *********************************** //
+
+        // with strategy design pattern
+
+        Context context = new Context(new Add());
+        System.out.println(context.doCal(x,y));
+
+        Context context1 = new Context(new Sub());
+        System.out.println(context1.doCal(x,y));
+
+        Context context2 = new Context(new Mul());
+        System.out.println(context2.doCal(x,y));
+
+        Context context3 = new Context(new Div());
+        System.out.println(context3.doCal(x,y));
+    }
+}
