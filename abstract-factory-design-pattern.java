7aa8c99cb9fd9bf9d5f99e052a72d8ed4cdec809
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2019-07-30 14:06:54 +0530

    Create abstract-factory-design-pattern.java

---
+++ abstract-factory-design-pattern.java
@@ -0,0 +1,106 @@
+package com.company;
+
+interface  Apperal{
+
+    void getDesign();
+}
+
+class BoyTouser implements Apperal{
+
+
+    @Override
+    public void getDesign(){
+        System.out.println("Boys Touser design ");
+    }
+
+}
+
+class LadiesTouser implements Apperal{
+    @Override
+    public void getDesign(){
+        System.out.println("Ladies Touser design ");
+    }
+
+}
+class BoysThrit implements Apperal{
+    @Override
+    public void getDesign(){
+        System.out.println("Boys Tshirt design ");
+    }
+
+}
+class LadiesTshirt implements Apperal{
+
+    @Override
+    public void getDesign(){
+
+        System.out.println("Ladies Tshirt design ");
+    }
+
+}
+
+interface CreateApperal {
+
+    Apperal generateApperal(String apperalType);
+}
+
+class CreateBoysApperal implements CreateApperal{
+
+    @Override
+    public Apperal generateApperal(String apperalType){
+
+        if(apperalType.equalsIgnoreCase("touser"))
+           return new BoyTouser();
+        else if(apperalType.equalsIgnoreCase("tshirt"))
+            return  new BoysThrit();
+
+        else
+            return null;
+    }
+
+}
+
+class CreateLadiesApperal implements CreateApperal{
+
+    @Override
+    public Apperal generateApperal(String apperalType){
+
+        if(apperalType.equalsIgnoreCase("touser")){
+           return new LadiesTouser();
+        }
+
+        else if(apperalType.equalsIgnoreCase("tshirt")) {
+            return new LadiesTshirt();
+        }
+        else
+            return null;
+    }
+
+}
+
+class CreateProducer{
+
+    public  static CreateApperal getApperalFactoey(boolean boy){
+
+        if(boy)
+            return new CreateBoysApperal();
+        else
+            return new CreateLadiesApperal();
+
+    }
+
+}
+
+
+public class ApperalMain {
+
+
+    public static void main(String[] args) {
+
+        CreateApperal createApperal = CreateProducer.getApperalFactoey(false);
+        createApperal.generateApperal("tshirt").getDesign();
+
+    }
+
+
+}
